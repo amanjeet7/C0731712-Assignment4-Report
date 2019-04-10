f954513d811e782fe3a2d2d98177102e81f67564
visualstudio <visualstudio@122-PC14>
2019-04-10 09:03:33 -0400

    starter code

--- c0731712/Program.cs
+++ c0731712/Program.cs
@@ -8,8 +8,42 @@ namespace c0731712
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+           
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+
+        }
+        public void run() { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+            using (StreamReader file = new StreamReader("u:/Users/731712.STUDENT.000/Downloads/Beowulf.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) !=null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has { counter} lines.");
+            }
+        }
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countspaces = 0;
+
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countletters++; }
+            }
+            return countspaces;
         }
     }
 }
